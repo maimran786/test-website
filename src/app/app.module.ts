579728c00d9ea30efe7b93c12c9154771a2786a1
maimran786 <mohammadabdulimran93@gmail.com>
2024-01-13 15:09:38 -0500

    Create Angular App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    UserComponent,
+    // NavComponent
   ],
   imports: [
     BrowserModule,
